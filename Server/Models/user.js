c7996415e6be0db5361888c4d8eb3e97eaaf5c62
favad <favadfavad2@gmail.com>
2024-01-26 15:57:44 +0530

    created user auth api functionalities

---
+++ Server/Models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userScheme = mongoose.Schema({
+    name: {
+        required: true,
+        type: String,
+        trim: true,
+    },
+    email: {
+        required: true,
+        type: String,
+        trim: true,
+        validate: {
+            validator: function (value) {
+                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                return value.match(re);
+            },
+            message: "Please enter a valid email address",
+        },
+    },
+    password: {
+        required: true,
+        type: String,
+        validate:{
+            validator: function (value){
+              return value.length > 6;
+            },
+            message: "Please enter a 6 character password",
+        }
+    },
+    address: {
+        type: String,
+        default: '',
+    },
+    type: {
+        type: String,
+        default: 'user',
+    },
+});
+
+const User = mongoose.model("User",userScheme);
+
+module.exports = User;
--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,37 @@
-console.log("hello,World");
-
+// IMPORT FROM PACKAGES
 const express = require("express");
 
+const mongoose = require('mongoose');
+
+// IMPORT FROM OTHER FILES
+const authRouter = require("./routes/auth");
+
+// INIT
 const PORT = 3000;
-
 const app = express();
+const DB = 'mongodb+srv://favad:favad123@cluster0.ciuwwr9.mongodb.net/?retryWrites=true&w=majority';
 
-// Creating An API
+// middleware
+app.use(express.json());
+app.use(authRouter);
 
- app.listen(PORT, "0.0.0.0", function (){
+// Connections
+mongoose.connect(DB).then(function () {
+    console.log("connection success");
+}).catch(function (e) {
+    console.log(e);
+});
+
+// CREATE GET API
+app.get("/hello-world", function (req, res) {
+
+    res.json({ hi: "Hello Favad" });
+
+});
+
+app.listen(PORT, "0.0.0.0", function () {
 
     console.log(`connected at port ${PORT} favad`);
 
 
- } )
+});
